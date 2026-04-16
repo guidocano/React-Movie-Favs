77c320450e4df5fc82408100c16278e4709ddf6d
Guido <guidocalciano@gmail.com>
2026-04-15 21:02:56 -0300

    completed search bar functionalities

--- react-movie-favs/src/pages/Home.jsx
+++ react-movie-favs/src/pages/Home.jsx
@@ -24,9 +24,24 @@ function Home() {
         loadPopularMovies()
     }, [])
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault()
-        alert(searchQuery)
+        if (!searchQuery.trim()) return 
+        if (loading) return 
+
+             setLoading(true)
+             try {
+                const searchResults = await searchMovies(searchQuery)
+                setMovies(searchResults)
+                setError(null)
+             } catch (err) {
+                console.log(err)
+                setError ("Failed to search movies.")
+             } finally {
+                setLoading(false)
+             }
+
+        // setSearchQuery("");
     }
 
     return <div className="home">
