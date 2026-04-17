47791c7ca4a3e194459559cd6e5daeaf0eb15730
Guido <guidocalciano@gmail.com>
2026-04-17 20:53:37 -0300

    finished last changes, favorites working

--- react-movie-favs/src/App.jsx
+++ react-movie-favs/src/App.jsx
@@ -2,13 +2,14 @@ import './css/App.css';
 import Favorites from './pages/Favorites';
 import Home from "./pages/Home";
 import {Routes, Route} from "react-router-dom";
+import { MovieProvider } from './contexts/MovieContext';
 import Navbar from "./components/NavBar";
 
 
 function App() {
 
   return (
-    <div>
+    <MovieProvider>
       <Navbar />
       <main className='main-content'>
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   )
 }
 
--- react-movie-favs/src/pages/Favorites.jsx
+++ react-movie-favs/src/pages/Favorites.jsx
@@ -1,6 +1,27 @@
 import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCards"
 
 function Favorites() {
+    const {favorites} = useMovieContext();
+
+    if (favorites) {
+        return (
+        <div className="favorites">
+            <h2>Your Favorites</h2>
+        <div className="movies-grid">
+            {favorites.map(
+                (eachmovie) => 
+                    (
+                // eachmovie.title.toLowerCase().startsWith(searchQuery) && (
+                
+                <MovieCard movie={eachmovie} key={eachmovie.id}/>
+                ))}
+        </div>
+        </div>
+        );
+    }
+
     return <div className="favorites-empty">
         <h2>No Favorite Movies Yet</h2>
         <p>Start adding movies to your favorites and they will appear here.</p>
